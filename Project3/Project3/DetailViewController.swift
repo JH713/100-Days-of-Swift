80d8c8db9c7c743bc2ef24ff2b33bd80aa69d911
JH713 <strider13@snu.ac.kr>
2023-12-12 20:46:34 +0900

    Feat: (Project3) Project1 가져옴

---
+++ Project3/Project3/DetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailViewController.swift
+//  Project3
+//
+//  Created by 이지현 on 12/12/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let selectedImage {
+            imageView.image = UIImage(named: selectedImage)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false 
+    }
+
+}
